d29e1434d1f4aa3f816b599de8ef741bb293c8e5
s220296 <s220296@students.aie.edu.au>
2023-02-24 10:24:42 +1100

    project linking setup

--- Azimuth-Test/Program.cs
+++ Azimuth-Test/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace Azimuth_Test
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			
+			
+			Console.Read();
+		}
+	}
+}
